4c1ff80017516430f7760bd9addc5413c8fe850f
rustam <b20128@students.iitmandi.ac.in>
2023-06-30 16:27:47 +0530

    #5: section and button transition animations

--- app.js
+++ app.js
@@ -13,6 +13,29 @@ function pageTransition(){
             this.className += ' active-btn';
         })
     }
+
+    // Section Active
+    for (var i = 0 ; i < allSections.length; i++) {
+        allSections[i].addEventListener('click', function (el) {
+            const id = el.target.dataset.id;
+            console.log(el.target)
+            if(id){
+                // Remove selected from the other btns
+                sectBtn.forEach((btn) => {
+                    btn.classList.remove('active')
+                });
+                
+                el.target.classList.add('active');
+    
+                //Hide other sections
+                sections.forEach((section) => {
+                    section.classList.remove('active');
+                });
+                const element = document.getElementById(id);
+                element.classList.add('active');
+            }
+        })
+     }
 }
 
 pageTransition();
